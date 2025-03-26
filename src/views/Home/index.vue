3567fe38ee16ad5285ccab1cbb62bed805b98fd6
LAPTOP-0QUIEHKP\kermit <232974759@qq.com>
2025-03-26 20:42:10 +0800

    home组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -7,6 +7,13 @@
     <home_new></home_new>
     <home_hot></home_hot>
     <home_product></home_product>
+    <!-- 封装面板组件 -->
+    <home_repanel title="新鲜好物" subtitle="新鲜出炉 品质靠谱">
+      <div>这里是新鲜好物的描述</div>
+    </home_repanel>
+    <home_repanel title="人气推荐" subtitle="人气推荐 物美价廉">
+      <div>没错是人气推荐的描述</div>
+    </home_repanel>
 </template>
 
 <script setup>
@@ -15,6 +22,7 @@ import home_banner from './class/home_banner.vue'
 import home_new from './class/home_new.vue'
 import home_hot from './class/home_hot.vue'
 import home_product from './class/home_product.vue'
+import home_repanel from './class/home_repanel.vue'
 </script>
 
 <style scoped>
